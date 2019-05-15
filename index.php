b585d32498b1a033567dc1482d7b19e261034f2a
KMordecai <48743434+KMordecai@users.noreply.github.com>
2019-05-14 20:22:01 -0400

    Index page
    Initial page of entry.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--
+Krystal Mordecai
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php include_once 'auth.php';?>
+    </body>
+</html>
